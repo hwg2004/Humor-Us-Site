b7811148e67ebdb3da885030249d49b2c467f91d
Harry <hwg35@cornell.edu>
2025-10-20 20:28:12 -0400

    Fixed quotation marks

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
       {/* Testimonial */}
       <section className="rounded-2xl bg-gradient-to-r from-purple-500 to-pink-500 text-white p-8 text-center shadow-xl">
         <p className="text-2xl font-medium mb-4 italic">
-          "HumorUs rules."
+          HumorUs rules.
         </p>
         <p className="text-lg">- Every HumorUs Member Ever</p>
       </section>
